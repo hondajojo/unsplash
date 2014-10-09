5110e8cb108fdc249a625d3b31c77aa9631fbfb6
hondajojo <cm1130335361@gmail.com>
2014-10-09 23:32:35 +0000

    add 注释

--- unsplash.py
+++ unsplash.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 #coding=utf-8
+#下载unsplash.com的图片
 import re
 import urllib2
 import urllib
@@ -11,13 +12,13 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 
 req = urllib2.Request(url,headers=headers)
 html = urllib2.urlopen(req).read()
 
-s = r'src="(.*?)"\sclass'
-picurl = re.findall(s,html)
+s = r'src="(.*?)"\sclass' #图片的正则i
+picurl = re.findall(s,html) #匹配图片的链接
 l = 1
 r = []
 for i in picurl:
-    p = i.split('amp;')
-    w = ''.join(itertools.chain(*p))
+    p = i.split('amp;')  #去除图片图片链接里的amp;
+    w = ''.join(itertools.chain(*p)) #将一个列表里的元素合并为一个字符串
     t = w.split('https://')
     y = ''.join(itertools.chain(*t))
     r.append(y)
